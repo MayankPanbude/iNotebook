0d11e841cbae53534924404d4920d8fa2352a604
MayankPanbude <92153909+MayankPanbude@users.noreply.github.com>
2024-09-18 15:02:13 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors')
+const path = require("path");
 
 connectToMongo();
 const app = express()
@@ -16,3 +17,21 @@ app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
